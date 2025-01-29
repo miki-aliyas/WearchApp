58633d0cd5edad9d8ed33b93bfdb320664667852
LAPTOP-M\81808 <miki.snoopy.729@gmail.com>
2025-01-29 23:51:39 +0900

    DetailFragmentのレイアウト、処理の実装途中

--- app/src/main/java/com/example/wearchapp/ui/detail/DetailFragment.java
+++ app/src/main/java/com/example/wearchapp/ui/detail/DetailFragment.java
@@ -8,12 +8,30 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.example.wearchapp.R;
 import com.example.wearchapp.ui.main.MainFragment;
+import com.example.wearchapp.ui.main.adapter.CarouselAdapter;
+import com.example.wearchapp.ui.main.adapter.RecommendationAdapter;
+import com.example.wearchapp.ui.topbar.TopBarActivity;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class DetailFragment extends Fragment {
 
+    private ViewPager2 viewPager;   //  カルーセルを表示する
+    private final List<View> pointerList = new ArrayList<>();   //  カルーセルのポイント（ドット）ビューのリスト
+    private CarouselAdapter carouselAdapter;    //  カルーセル表示を行うためのアダプタ
+    private static final int CARD_WIDTH_DP = 140;   // カードの横幅（dp）
+    private static final int CARD_HEIGHT_DP = 140;  // カードの縦幅（dp）
+    private static final List<Integer> INT_DATA_LIST = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);   // カテゴリリストのデータ
+    private RecyclerView recyclerView;  //  カテゴリリストを表示する
+    private RecommendationAdapter adapter;  //  カテゴリリストを表示するためのアダプタ
     public static Fragment newInstance() {
         return new DetailFragment();
     }
@@ -26,6 +44,27 @@ public class DetailFragment extends Fragment {
     @Override   //  ビューの作成が完了した後にビューを呼び出す
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        //画面の処理
+
+        // ViewPagerの取得
+        viewPager =view.findViewById(R.id.viewPager);
+
+        // XMLレイアウトのIDからRecyclerViewの取得
+        recyclerView = view.findViewById(R.id.recyclerView);
+    }
+
+    private void recyclerViewSettings(){    //  RecyclerViewの設定
+        GridLayoutManager layoutManager = new GridLayoutManager(
+                requireContext(),
+                2,
+                GridLayoutManager.VERTICAL,
+                false
+        );
+        float dp = getResources().getDisplayMetrics().density;
+        // Adapterの生成（インスタンス化）
+        adapter = new RecommendationAdapter(INT_DATA_LIST, (int)(CARD_WIDTH_DP * dp), (int)(CARD_HEIGHT_DP * dp));
+        // RecyclerViewにAdapterを設定
+        recyclerView.setAdapter(adapter);
+        // RecyclerViewにLayoutManagerを設定
+        recyclerView.setLayoutManager(layoutManager);
     }
 }
